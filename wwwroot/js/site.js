d8d46e0eb46f8c90ade9df3fac09831e76f55315
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-08-27 06:55:58 +0800

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(() => {
+    $("[name='form-visibility-cb']").bootstrapSwitch();
+    $("[name='form-visibility-cb']").on('switchChange.bootstrapSwitch', (e, state) => {
+        const formVisibilityState = $("[name='form-visibility-cb']").bootstrapSwitch('state');
+        console.log(`[debug] formVisibilityState=${formVisibilityState}`);
+        console.log(`[debug] state=\n`, state);
+        if (state) {
+            $("#detail-form").show();
+        } else {
+            $("#detail-form").hide();
+        }
+    });
+});
